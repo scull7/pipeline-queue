0fa88a3faa170e00eeec65d7a27c9d059c4658ec
Nathan Sculli <nathan.sculli@allegiantair.com>
2014-05-04 18:52:09 -0700

    Add in tests for the Entry#run() function.

--- test/lib/entry.spec.js
+++ test/lib/entry.spec.js
@@ -71,6 +71,63 @@ describe('Entry', function () {
     expect(entry).to.have.property('run').that.is.a('function');
   });
 
+  describe('#run()', function () { 
+    var clock, now;
+
+    beforeEach(function () {
+      now = (new Date()).getTime();
+      clock = sinon.useFakeTimers(now);  
+    });
+    afterEach(function() {
+      clock.restore();
+    });
+    it('should return the entry object.', function () {
+      expect(entry.run()).to.be.eql(entry);
+    });
+    it('should set the active flag to zero', function () {
+      entry.active = 1;
+      entry.run();
+      expect(entry.active).to.be.eql(0);
+    });
+    it('should set the last repsonse to the arguments passed', function () {
+      entry.run('test1','arg2','arg3');
+      expect(entry.last).to.be.eql(['test1','arg2','arg3']);
+    });
+    it('should set the time to now', function () {
+      entry.run();
+      expect(entry.time).to.be.eql(now);
+    });
+    it('should call the handler for each callback in the queue.', function () {
+      var spy1 = sinon.spy(),
+      spy2 = sinon.spy(),
+      spy3 = sinon.spy();
+      
+      entry.add(spy1).add(spy2).add(spy3);
+
+      entry.run('test1','test2');
+      spy1.should.have.been.calledWith('test1','test2');
+      spy2.should.have.been.calledWith('test1','test2');
+      spy3.should.have.been.calledWith('test1','test2');
+    });
+  });
+
+  it('should have a getHandler function', function () {
+    expect(entry).to.have.property('getHandler').that.is.a('function');
+  });
+
+  describe('#getHandler()', function () {
+    it('should return a function', function () {
+      var handler = entry.getHandler();
+      expect(handler).to.be.a('function');
+    });
+    it('should return a function that proxys arguments to the passed function argument.', function () {
+      var handler = entry.getHandler(['arg1','arg2','arg3']);
+      var spy = sinon.spy();
+      handler(spy);
+      spy.should.have.been.calledWith('arg1','arg2','arg3');
+    });
+  });
+
   it('should have an getLast function', function () {
     expect(entry).to.have.property('getLast').that.is.a('function');
   });
